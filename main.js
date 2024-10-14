95fd89e9b4dc662adc07b4d80bc28f6c3ad88911
guilherme <guilhermeppardini@gmail.com>
2024-10-14 14:48:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//start 
+
+//main program loop
+
+let flag = true;
+
+while(flag)
+{
+    let userInput = prompt("COMANDO:");
+    console.log("voce escreveu:", userInput);
+    if(userInput === 'end');
+        flag = false;
+}
